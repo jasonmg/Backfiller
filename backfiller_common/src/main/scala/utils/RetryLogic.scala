dbca59a1d4a8a361251447629b6d52fa48eb4b2b
Jason Ji <ji_mingjiang@163.com>
2016-08-02 08:20:26 +0800

    retry logic implemention

--- backfiller_common/src/main/scala/utils/RetryLogic.scala
+++ backfiller_common/src/main/scala/utils/RetryLogic.scala
@@ -1,12 +1,26 @@
 package main.scala.utils
 
+import scala.util.control.NonFatal
 
-object RetryLogic {
 
+object RetryLogic extends Log {
 
-  // TODO  has to implement in the future
-  def retry[In, Out](arg: In, f: In => Out, tryTime: Int = 5): Out = {
-    f(arg)
+  def retry[In, Out](arg: In, f: In => Out, tried: Int = 0, maxReTry: Int = 5, interval: Long = 30000): Out = {
+    try {
+      f(arg)
+    } catch {
+      case NonFatal(ex) =>
+        if (tried >= maxReTry) {
+          log.error(s"tried time: $tried >= $maxReTry, thus throw exception.")
+          throw ex
+        } else {
+          val intervalNext = interval + 10000 * tried
+          log.warn(s"retry failed: ${tried} times, max: ${maxReTry}, sleep: ${intervalNext}ms before next try")
+          Thread.sleep(intervalNext)
+          retry(arg, f, tried + 1, maxReTry, intervalNext)
+        }
+      case e @ _ => log.fatal("Fatal error occurred, pay attention!"); throw e
+    }
   }
 
   def tryOnce[Out](f: => Out): Out = {
